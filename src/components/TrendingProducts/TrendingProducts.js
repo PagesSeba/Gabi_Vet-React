64fb79c405f1b7949c8c14c6434d6b6e2c040401
PagésSebastian <sebapages2@gmail.com>
2022-11-18 15:45:04 -0300

    TrendingProducts.js

---
+++ src/components/TrendingProducts/TrendingProducts.js
@@ -0,0 +1,107 @@
+import React, {useEffect, useState} from 'react'
+import Item from '../Item/Item'
+import {Link, useParams} from "react-router-dom"
+import Box from '@mui/material/Box';
+import CircularProgress from '@mui/material/CircularProgress';
+import { collection, getDocs, orderBy, limit, query} from "firebase/firestore"
+import db from "../../firebase"
+import "../ItemListContainer/ItemListContainer.css"
+
+
+
+function TrendingProducts() {
+    const { destacado } = useParams();
+    const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    const getProducts = async () => {
+        const itemsCollection = query(collection(db, "productos"), orderBy("nombre", "asc"))
+        console.log(itemsCollection)
+        const productosSnapshot = await getDocs(itemsCollection)
+        const productList = productosSnapshot.docs.map((doc) => {
+            let product = doc.data()
+            product.id = doc.id
+            return product
+        })
+        return productList 
+    
+    }
+
+    
+    
+    useEffect(() => {
+        setLoading(true)
+        setProducts([])
+        return getProducts().then((productos) => {
+            setLoading(false)
+            destacado ? filterProductByTrending(productos, destacado) : setProducts(productos)
+            
+            
+            
+        })
+
+    }, [destacado])
+
+    const filterProductByTrending = (array, destacado) => {
+        array.map((producto)=> {
+            if (destacado == producto.destacado){
+                return setProducts( products => [...products, producto])
+            }
+        })
+
+
+    }
+    
+
+
+
+
+    
+    return (
+        <div>
+        <div className="cInicio">
+            {
+                !loading ? (
+                    
+                    products.map((producto) => {
+                        const { id, nombre, precio, info, img, stock, marca, destacado } = producto;
+                        if (destacado) {
+                            return (
+                            
+                                <Link className='cardM linkNV negro' to={`./${id}`}>
+                                <div key={id} className="sombras" >
+                                    <Item
+                                        nombre={nombre}
+                                        precio={precio}
+                                        info={info}
+                                        img={`/assets/images/${img}`}
+                                        stock={stock}
+                                        marca= {marca}
+    
+                                    />
+                                   
+                                </div>
+                                </Link>
+                            )}
+                        }
+
+
+
+
+                    )
+
+                ) : (
+                    <Box sx={{ display: 'flex' }} className="loading">
+                        <CircularProgress />
+                    </Box>
+                )
+            }
+
+        </div>
+        </div>
+
+
+    )
+}
+
+export default TrendingProducts
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,11 @@
 import React from "react";
-import ItemListContainer from "../components/ItemListContainer/ItemListContainer"
-import EmblaCarousel from "../components/Carousel/js/EmblaCarousel";
 import "../components/Carousel/css/embla.css"
 import WppWidget from "../components/WppWidget/WppWidget";
-import ItemCarousel from "../components/ItemCarousel/ItemCarousel";
-import Carousel from "react-elastic-carousel";
 import "../components/ItemCarousel/ItemCarousel.css";
-import Item from "../components/Item/Item";
 import CardCategory from "../components/CardCategory/CardCategory";
 import SectionInfo from "../components/SectionInfo/SectionInfo";
 import HeroSection from "../components/HeroSection/HeroSection";
-
-const SLIDE_COUNT = 4;
-const slides = Array.from(Array(SLIDE_COUNT).keys());
-const breakPoints = [
-    { width: 1, itemsToShow: 1 },
-    { width: 550, itemsToShow: 2 },
-    { width: 768, itemsToShow: 3 },
-    { width: 1200, itemsToShow: 4 },
-  ];
+import TrendingProducts from "../components/TrendingProducts/TrendingProducts";
 
 const Home = () => {
     
@@ -27,7 +14,7 @@ const Home = () => {
             <WppWidget />
             <HeroSection />
             <h3 className="mt-4">Destacado</h3>
-            <ItemListContainer />
+            <TrendingProducts />
             <CardCategory />
             <SectionInfo />
         </div>
